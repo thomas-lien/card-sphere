5e3dcedb8eae0c79930a2002f594bbc0f6fe3959
Sean Hoang <sbhoang@uci.edu>
2025-04-26 22:39:56 -0700

    simple commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="w-full px-4 flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger asChild className="lg:hidden">
